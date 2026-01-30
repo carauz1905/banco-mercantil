22c8ee982c664177e5a58e23b0013ef1ef40a878
Carlos Arauz <arauz.carlos@hotmail.com>
2026-01-30 13:16:56 -0500

    Tour 360 Banco Mercantil

---
+++ data.js
@@ -0,0 +1,782 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sala-1",
+      "name": "Sala 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -2.270586775234632,
+        "pitch": 0.20088236852051722,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.997205901664149,
+          "pitch": 0.1466230902969503,
+          "rotation": 0,
+          "target": "1-sala-2-y-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.8401095497634632,
+          "pitch": 0.21233102581881091,
+          "title": "Sala 1",
+          "text": "Piso 21<div><br></div><div>Uso: Sala de reuniones mediana&nbsp;</div><div>Capacidad: 6 u 8 personas</div><div><br></div><div>Dimensiones aproximadas:</div><div>Largo: 4.2 m</div><div>Ancho: 3.8 m</div><div>Alto: 3.5 m</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-sala-2-y-3",
+      "name": "Sala 2 y 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": 1.8997185391166829,
+        "pitch": 0.08356462120875463,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2974692881309178,
+          "pitch": 0.020863537000666454,
+          "rotation": 0,
+          "target": "2-sala-4-5-6-7"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3414349430804169,
+          "pitch": 0.0694796508312372,
+          "title": "Sala 2 y 3",
+          "text": "Piso 21<div><br></div><div>Uso: Sala de reuniones</div><div>Capacidad: 10 a 12 personas</div><div><br></div><div>Dimensiones aproximadas:</div><div>Largo: 6.0 m</div><div>Ancho: 4.0 m</div><div>Alto: 3.5 m</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-sala-4-5-6-7",
+      "name": "Sala 4, 5, 6, 7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -0.19065215142478564,
+        "pitch": 0.5317888267378379,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1384591940769475,
+          "pitch": -0.05113078488930789,
+          "rotation": 0,
+          "target": "3-sala-presidencia"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1719086365369655,
+          "pitch": 0.050200445855775655,
+          "title": "Salas 4, 5, 6, 7",
+          "text": "Piso 21<div><br></div><div>Uso: Sala de reuniones pequeñas</div><div>Capacidad: 4 personas</div><div><br></div><div>Dimensiones aproximadas:</div><div>Largo: 3.4 m</div><div>Ancho: 2.8 m</div><div>Alto: 3.5 m</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-sala-presidencia",
+      "name": "Sala Presidencia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -1.8438030738134152,
+        "pitch": 0.116849923833378,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1166427066940017,
+          "pitch": -0.14731932993873542,
+          "rotation": 0,
+          "target": "4-sala-presidencia-detalle-techo"
+        },
+        {
+          "yaw": 3.1023546294727424,
+          "pitch": 0.023257225035784757,
+          "rotation": 0,
+          "target": "7-sala-directiva"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.987310005412894,
+          "pitch": 0.009175836148550331,
+          "title": "Sala Presidencia",
+          "text": "Piso 23<div><br></div><div>Uso: Sala de Presidencia</div><div>Capacidad: 10 personas</div><div><br></div><div>Dimensiones aproximadas:</div><div>Largo: 7.6 m</div><div>Ancho: 4.8 m</div><div>Doble altura</div><div>Alto A: 2.2 m</div><div>Alto B: 3.7 m</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-sala-presidencia-detalle-techo",
+      "name": "Sala Presidencia, detalle techo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -1.3055971132756419,
+        "pitch": 0.5635244454634218,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9250131524486491,
+          "pitch": -0.09142448657703994,
+          "rotation": 0,
+          "target": "5-sala-presidencia--desnivel-techo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3055971132756419,
+          "pitch": 0.5635244454634218,
+          "title": "Techo Doble Altura Sala de Presidencia",
+          "text": "Problemas de acustica<div>Ancho: 1.2 m</div><div>Altura 1.5 m</div>"
+        }
+      ]
+    },
+    {
+      "id": "5-sala-presidencia--desnivel-techo",
+      "name": "Sala Presidencia , desnivel techo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -0.04902133783420837,
+        "pitch": 0.2478474530775685,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2579049199919403,
+          "pitch": 0.5128520928886839,
+          "rotation": 0,
+          "target": "6-sala-presidencia-conexines-debajo-de-la-mesa"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9681532229993906,
+          "pitch": -0.11381059598500265,
+          "title": "Otra Vista del Desnivel del techo",
+          "text": "es a lo largo de toda la sala, se pierde el audio y conexión al moverse a esta area"
+        }
+      ]
+    },
+    {
+      "id": "6-sala-presidencia-conexines-debajo-de-la-mesa",
+      "name": "Sala Presidencia, conexines debajo de la mesa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -0.2931843042707065,
+        "pitch": -0.6477171682948057,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9967725107253838,
+          "pitch": -0.10993401138055958,
+          "rotation": 0,
+          "target": "3-sala-presidencia"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.453025685750994,
+          "pitch": -0.5556674711592784,
+          "title": "Conexiones debajo de la mesa",
+          "text": "se cuenta con pequeña bandeja"
+        }
+      ]
+    },
+    {
+      "id": "7-sala-directiva",
+      "name": "Sala Directiva",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -1.4325455522695023,
+        "pitch": 0.16034843376982266,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3499952644055924,
+          "pitch": 0.36569541877111966,
+          "rotation": 0,
+          "target": "9-sala-directiva-conexiones-debajo-de-la-mesa"
+        },
+        {
+          "yaw": 1.1642757419508243,
+          "pitch": 0.033325587957238056,
+          "rotation": 0,
+          "target": "8-sala-directiva-sistema-actual"
+        },
+        {
+          "yaw": 1.8917323235465435,
+          "pitch": 0.10068044831475831,
+          "rotation": 0,
+          "target": "10-sala-ampliable-completa"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5302834473551865,
+          "pitch": 0.03273873934784888,
+          "title": "Sala Directiva",
+          "text": "Piso 23<div><br></div><div>Uso: Sala de reuniones de Junta Directiva</div><div>Capacidad: 18 a 26 personas</div><div><br></div><div>Dimensiones aproximadas:</div><div>Largo: 10.6 m</div><div>Ancho: 6.4 m</div><div>Alto: 2.7 m</div>"
+        }
+      ]
+    },
+    {
+      "id": "8-sala-directiva-sistema-actual",
+      "name": "Sala Directiva, sistema actual",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4158540443233516,
+          "pitch": 0.17176697221917436,
+          "rotation": 0,
+          "target": "9-sala-directiva-conexiones-debajo-de-la-mesa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-sala-directiva-conexiones-debajo-de-la-mesa",
+      "name": "Sala Directiva, conexiones debajo de la mesa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-sala-ampliable-completa",
+      "name": "Sala Ampliable completa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": 1.518238286975488,
+        "pitch": 0.09179774343467884,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7486458022357905,
+          "pitch": 0.1140318432241525,
+          "rotation": 4.71238898038469,
+          "target": "11-sala-ampliable-a"
+        },
+        {
+          "yaw": 2.04444195194971,
+          "pitch": 0.12987726899872776,
+          "rotation": 7.853981633974483,
+          "target": "13-sala-ampliable-b"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.391721967113634,
+          "pitch": 0.18943475892611872,
+          "title": "Sala ampliable completa",
+          "text": "Piso 12<div><br></div><div>Uso: Salón de capacitación dividible, se convierte en dos con un panel en esta zona</div><div><br></div><div>Dimensiones aproximadas:</div><div>Largo: 20.4 m</div><div>Ancho: 5.8 m</div><div>Alto: 3.8 m</div>"
+        }
+      ]
+    },
+    {
+      "id": "11-sala-ampliable-a",
+      "name": "Sala Ampliable A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": 2.9114795362729975,
+        "pitch": -0.2684790584047967,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.411591450108503,
+          "pitch": -0.2989145319618789,
+          "rotation": 0,
+          "target": "12-sala-ampliable-a-proyector-y-pantalla"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-sala-ampliable-a-proyector-y-pantalla",
+      "name": "Sala Ampliable A, Proyector y pantalla",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -0.47728309909169475,
+        "pitch": 0.005088522015711305,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6625005436872478,
+          "pitch": 0.14329120704008247,
+          "rotation": 0,
+          "target": "13-sala-ampliable-b"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2926186516406748,
+          "pitch": -0.04604243434603994,
+          "title": "Detalle de Sala A",
+          "text": "Pantalla retraible con Proyector"
+        }
+      ]
+    },
+    {
+      "id": "13-sala-ampliable-b",
+      "name": "Sala Ampliable B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -0.3151204620877692,
+        "pitch": -0.1000894704201194,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.16189265084048543,
+          "pitch": -0.6538295579967404,
+          "rotation": 0,
+          "target": "14-sala-ampliable-b-detalle-de-microfono-aereo-y-proyector"
+        },
+        {
+          "yaw": 1.932557943056728,
+          "pitch": -0.011929170517198884,
+          "rotation": 1.5707963267948966,
+          "target": "15-gabinete"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-sala-ampliable-b-detalle-de-microfono-aereo-y-proyector",
+      "name": "Sala Ampliable B, detalle de microfono aereo y proyector",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9854622100593895,
+          "pitch": 0.2517360722860218,
+          "rotation": 1.5707963267948966,
+          "target": "15-gabinete"
+        },
+        {
+          "yaw": 1.245135029652987,
+          "pitch": 0.09512387701546743,
+          "rotation": 0,
+          "target": "11-sala-ampliable-a"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.1392725508692845,
+          "pitch": 0.10601073991410992,
+          "title": "Detalle de techo",
+          "text": "microfono y proyector aereo"
+        }
+      ]
+    },
+    {
+      "id": "15-gabinete",
+      "name": "Gabinete",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Banco Mercantil",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
